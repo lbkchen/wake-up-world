3f9d79c73b683e226d5f31aa779f6c4b9b888d5c
Casey <kaze@berkeley.edu>
2016-04-09 02:32:00 -0700

    hi

---
+++ wakeupworld/wakeupworld/PatientsCell.swift
@@ -0,0 +1,13 @@
+//
+//  PatientsCell.swift
+//  wakeupworld
+//
+//  Created by Casey Takeda on 4/9/16.
+//  Copyright © 2016 wakeup. All rights reserved.
+//
+
+import Cocoa
+
+class PatientsCell: UITableViewCell {
+
+}
---
+++ wakeupworld/wakeupworld/PatientsList.swift
@@ -0,0 +1,13 @@
+//
+//  PatientsList.swift
+//  wakeupworld
+//
+//  Created by Casey Takeda on 4/9/16.
+//  Copyright © 2016 wakeup. All rights reserved.
+//
+
+import Cocoa
+
+class PatientsList: UITableViewController {
+
+}
